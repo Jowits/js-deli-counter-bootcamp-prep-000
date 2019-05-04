8bf4a8b39d01c050098f89fb62227111659b895e
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-04 14:30:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var katzDeliLine = [];
-
-function takeANumber(line, name){
-  line.push(name);
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+var count = 0;
+function takeANumber(line){
+  count += 1;
+  line.push(count);
+  return `Welcome, You are number ${line.length}.`
 }
 function nowServing(line){
-  if (!line.length){
+  if (line.length === 0){
     return "There is nobody waiting to be served!";
   }
   return `Currently serving ${line.shift()}.`
 }
 
 function currentLine(line){
-  if (!line.length){
+  if (line.length === 0){
     return "The line is currently empty.";
   }
-  const numbersAndNames = []
+  var numbersAndNames = []
 
   for (let i = 0, l = line.length; i < l; i++) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`)
